6aa6278a50f440e7e16866912d1b54f027834a3c
deliteser112 <deliteser112@gmail.com>
2024-03-10 13:47:07 +0100

    init commit

---
+++ src/routes/index.js
@@ -0,0 +1,120 @@
+import { Suspense, lazy } from 'react';
+import { Navigate, useRoutes, useLocation } from 'react-router-dom';
+// layouts
+import MainLayout from '../layouts/main';
+import DashboardLayout from '../layouts/dashboard';
+import LogoOnlyLayout from '../layouts/LogoOnlyLayout';
+// guards
+import GuestGuard from '../guards/GuestGuard';
+// import AuthGuard from '../guards/AuthGuard';
+// import RoleBasedGuard from '../guards/RoleBasedGuard';
+// config
+// import { PATH_AFTER_LOGIN } from '../config';
+
+// components
+import LoadingScreen from '../components/LoadingScreen';
+
+// ----------------------------------------------------------------------
+
+const Loadable = (Component) => (props) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { pathname } = useLocation();
+
+  return (
+    <Suspense fallback={<LoadingScreen isDashboard={pathname.includes('/dashboard')} />}>
+      <Component {...props} />
+    </Suspense>
+  );
+};
+
+export default function Router() {
+  return useRoutes([
+    {
+      path: 'auth',
+      children: [
+        {
+          path: 'login',
+          element: (
+            <GuestGuard>
+              <Login />
+            </GuestGuard>
+          ),
+        },
+        {
+          path: 'register',
+          element: (
+            <GuestGuard>
+              <Register />
+            </GuestGuard>
+          ),
+        },
+        { path: 'login-unprotected', element: <Login /> },
+        { path: 'register-unprotected', element: <Register /> },
+        { path: 'reset-password', element: <ResetPassword /> },
+        { path: 'new-password', element: <NewPassword /> },
+        { path: 'verify', element: <VerifyCode /> },
+      ],
+    },
+    // Main Routes
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        { element: <HomePage />, index: true },
+        { path: 'about-us', element: <About /> },
+      ],
+    },
+
+    // Dashboard Routes
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/one" replace />, index: true },
+        { path: 'one', element: <PageOne /> },
+        { path: 'two', element: <PageTwo /> },
+        { path: 'three', element: <PageThree /> },
+        {
+          path: 'user',
+          children: [
+            { element: <Navigate to="/dashboard/user/four" replace />, index: true },
+            { path: 'four', element: <PageFour /> },
+            { path: 'five', element: <PageFive /> },
+            { path: 'six', element: <PageSix /> },
+          ],
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: '404', element: <NotFound /> },
+        { path: '*', element: <Navigate to="/404" replace /> },
+      ],
+    },
+    { path: '*', element: <Navigate to="/404" replace /> },
+  ]);
+}
+
+// AUTHENTICATION
+const Login = Loadable(lazy(() => import('../pages/auth/Login')));
+const Register = Loadable(lazy(() => import('../pages/auth/Register')));
+const ResetPassword = Loadable(lazy(() => import('../pages/auth/ResetPassword')));
+const NewPassword = Loadable(lazy(() => import('../pages/auth/NewPassword')));
+const VerifyCode = Loadable(lazy(() => import('../pages/auth/VerifyCode')));
+
+// Dashboard
+const PageOne = Loadable(lazy(() => import('../pages/PageOne')));
+const PageTwo = Loadable(lazy(() => import('../pages/PageTwo')));
+const PageThree = Loadable(lazy(() => import('../pages/PageThree')));
+const PageFour = Loadable(lazy(() => import('../pages/PageFour')));
+const PageFive = Loadable(lazy(() => import('../pages/PageFive')));
+const PageSix = Loadable(lazy(() => import('../pages/PageSix')));
+const NotFound = Loadable(lazy(() => import('../pages/Page404')));
+
+// MAIN
+const HomePage = Loadable(lazy(() => import('../pages/Home')));
+const About = Loadable(lazy(() => import('../pages/About')));
+// const Maintenance = Loadable(lazy(() => import('../pages/Maintenance')));
+// const ComingSoon = Loadable(lazy(() => import('../pages/ComingSoon')));
